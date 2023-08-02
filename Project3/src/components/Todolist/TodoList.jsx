f55a68dbf90d6b85e93b4d7fc7f986e8a2c99a46
codespace555 <namansanjaykumar@gmail.com>
2023-08-02 22:33:45 +0530

    all done

--- Project3/src/components/Todolist/TodoList.jsx
+++ Project3/src/components/Todolist/TodoList.jsx
@@ -1,29 +1,34 @@
 import "./todolist.css";
 import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 
-
-function TodoList({ todo }) {
+function TodoList({ todo, index , onDelete}) {
   const [StatusChanges, setStatusChanges] = useState("pending");
   let changes = () => {
-  if (StatusChanges === "pending") {
-    setStatusChanges("Complete");
-    
-  }else{
-    setStatusChanges("pending");
-  }
-  
+    if (StatusChanges === "pending") {
+      setStatusChanges("Complete");
+      toast("Congratulations ");
+    } else {
+      setStatusChanges("pending");
+      toast("Hurry Up");
+    }
   };
+  
   return (
     <>
       <div className="item">
-        <div className="list">{todo}</div>
+        <div className="list">
+          <span className="index"></span>
+
+          <span>{todo}</span>
+        </div>
         <div className="status_button">
           <h4>
-            Status:<span className="status">   {StatusChanges}</span>
+            Status:<span className="status"> {StatusChanges}</span>
           </h4>
           <div className="update_btn">
             <button onClick={changes}>Update Status</button>
-            <button>Remove</button>
+            <button onClick ={() => onDelete(index)}>Remove</button>
           </div>
         </div>
       </div>
